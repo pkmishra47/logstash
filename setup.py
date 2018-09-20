00bde1a066575a79b0276186b432cf5001f79656
PradeepMishra <pradeep.kumar@travelyaari.com>
2018-09-20 23:38:50 +0530

    minor commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="logash_test",
+    name="logstash_test",
     version="0.0.1",
     author="Pradeep Mishra",
     author_email="pkmishra.pradeep@gmail.com",
